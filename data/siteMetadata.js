a1a27f9082250a3a37c6dc1e20d37d49c794838d
Daniel Arday <danielarday@daniels-mbp.myfiosgateway.com>
2023-12-27 17:00:10 -0500

    Updated a blog post

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -3,7 +3,7 @@ const siteMetadata = {
   title: 'Next.js Starter Blog',
   author: 'David Arday',
   headerTitle: 'Prescription For Life Blog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  description: 'Representing Altera Healthtec',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
@@ -11,7 +11,7 @@ const siteMetadata = {
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
   mastodon: 'https://mastodon.social/@mastodonuser',
-  email: 'address@yoursite.com',
+  email: 'darday@rx2live.com',
   github: 'https://github.com',
   twitter: 'https://twitter.com/Twitter',
   facebook: 'https://facebook.com',
