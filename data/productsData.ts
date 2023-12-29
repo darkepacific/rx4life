c4cc0605ceff5ad2bfc8b5dafcfc169dfa87e53e
Daniel Arday <danielarday@Daniels-MacBook-Pro.local>
2023-12-29 18:45:08 -0500

    Added BHS to products

--- data/productsData.ts
+++ data/productsData.ts
@@ -1,18 +1,18 @@
 const productsData = [
+  // {
+  //   title: 'A Search Engine',
+  //   description: `What if you could look up any information in the world? Webpages, images, videos
+  //   and more. Google has many features to help you find exactly what you're looking
+  //   for.`,
+  //   imgSrc: '/static/images/google.png',
+  //   href: 'https://www.google.com',
+  // },
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
+    title: 'Behavioral Health Services',
     description: `Imagine being able to travel back in time or to the future. Simple turn the knob
     to the desired date and press "Go". No more worrying about lost keys or
     forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
+    imgSrc: '/static/images/bhs-hero.png',
     href: '/blog/the-time-machine',
   },
 ]
