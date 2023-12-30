5e618e8d964bbecc731598e1af45886a22582cfe
Daniel Arday <danielarday@daniels-mbp.myfiosgateway.com>
2023-12-30 17:38:10 -0500

    Added in images

--- app/Main.tsx
+++ app/Main.tsx
@@ -12,7 +12,7 @@ export default function Home({ posts }) {
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Latest
+            A New Approach to Healthcare
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
             {siteMetadata.description}
@@ -21,7 +21,7 @@ export default function Home({ posts }) {
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
           {!posts.length && 'No posts found.'}
           {posts.slice(0, MAX_DISPLAY).map((post) => {
-            const { slug, date, title, summary, tags } = post
+            const { slug, date, title, summary, tags, images } = post
             return (
               <li key={slug} className="py-12">
                 <article>
@@ -30,6 +30,13 @@ export default function Home({ posts }) {
                       <dt className="sr-only">Published on</dt>
                       <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
                         <time dateTime={date}>{formatDate(date, siteMetadata.locale)}</time>
+                      </dd>
+                      <dd>
+                        <img
+                          src={`${images}`}
+                          alt={title}
+                          className="w-auto h-auto max-w-full max-h-60 object-cover" // Adjust size as appropriate
+                        />
                       </dd>
                     </dl>
                     <div className="space-y-5 xl:col-span-3">
