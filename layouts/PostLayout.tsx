6e4c712226e3875594870da742df49c0e0a0836b
Daniel Arday <danielarday@daniels-mbp.myfiosgateway.com>
2023-12-30 17:03:54 -0500

    Updated Discuss URL'

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -12,8 +12,7 @@ import ScrollTopAndComment from '@/components/ScrollTopAndComment'
 
 const editUrl = (path) => `${siteMetadata.siteRepo}/blob/main/data/${path}`
 const discussUrl = (path) =>
-  `https://mobile.twitter.com/search?q=${encodeURIComponent(`${siteMetadata.siteUrl}/${path}`)}`
-  // `https://www.linkedin.com/in/david-arday-md-mph-456b1a4/`  
+  `https://mobile.twitter.com/search?q=${encodeURIComponent(`${siteMetadata.siteUrl}/${path}`)}`  
 
 const postDateTemplate: Intl.DateTimeFormatOptions = {
   weekday: 'long',
