fe0963602dfde052357f491db33bbd39d8c70d3d
Daniel Arday <danielarday@daniels-mbp.myfiosgateway.com>
2023-12-31 21:03:31 -0500

    Updates

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -23,7 +23,7 @@ export default function Footer() {
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
           <Link href="https://www.alterahealthtec.com/">
-            Part of Altera Healtec
+            Altera Healthtec
           </Link>
         </div>
       </div>
