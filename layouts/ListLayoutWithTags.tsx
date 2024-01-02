378613d153ff26246db463e3d76e396667af9ab0
Daniel Arday <danielarday@daniels-mbp.myfiosgateway.com>
2024-01-01 23:40:19 -0500

    Removed tags

--- layouts/ListLayoutWithTags.tsx
+++ layouts/ListLayoutWithTags.tsx
@@ -80,7 +80,8 @@ export default function ListLayoutWithTags({
       <div>
         <div className="pb-6 pt-6">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:hidden sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            {title}
+            {/* {title} */}
+            Tags
           </h1>
         </div>
         <div className="flex sm:space-x-24">
